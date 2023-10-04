b6b1a4e1707066a6c6e110e98fd27d5a12bf3f85
ev0clu <ev0clu@gmail.com>
2023-10-04 13:51:25 +0200

    Frontend sign-in and sign-up pages are added

---
+++ frontend/src/main.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+import App from './App.tsx';
+import '../src/assets/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './pages/Home.tsx';
+import LoginPage from './pages/Login.tsx';
+import RegisterPage from './pages/Register.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
