1b43a9bdb6bc973379230589e6a8103fd4a696fb
ev0clu <ev0clu@gmail.com>
2023-10-11 15:36:50 +0200

    Title is updated

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -39,7 +39,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>MERN Blog</Navbar.Brand>
+            <Navbar.Brand>MERN Auth</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
